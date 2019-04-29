e219f06055e4d1352542af23a0c49514cd8686d6
dixinkai <dixinkai@outlook.com>
2019-04-29 15:41:50 +0000

    update

--- Image_Processing/pic_smooth_texture.cu
+++ Image_Processing/pic_smooth_texture.cu
@@ -13,11 +13,11 @@ __global__
 void smooth_kernel(char *img, int width, int height, int channels){
   unsigned int x = blockIdx.x * blockDim.x + threadIdx.x; 
   unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-  unsigned int offset = x + y * blockDim.x * gridDim.x;
+  //unsigned int offset = x + y * blockDim.x * gridDim.x;
 
-  //归一化
-  float u = x/(float)width;
-  float v = y/(float)height;
+  //归一化时候的情况
+  //float u = x/(float)width;
+  //float v = y/(float)height;
 
   //获取纹理内存中的输入图像信息
   float4 pixel    = tex2D(tex,x,y);
@@ -34,19 +34,51 @@ void smooth_kernel(char *img, int width, int height, int channels){
 }
 
 int main(){
-  cv::Mat src = imread("lena.jpg",IMREAD_COLOR);
-  cv::resize(src, src, Size(256, 256));
+  cv::Mat src = imread("lena.jpg",cv::IMREAD_COLOR);
+  cv::resize(src, src, cv::Size(256, 256));
   cvtColor(src, src, CV_BGR2BGRA);
   int width = src.rows;
   int height = src.cols;
   int channels = src.channels();
-  int imgsize
-  int width * height * channels;
+  int imgsize = width * height * channels;
 
   //定义cuda数组
-  cudaArray *cudaArray;
-  cudaChannelFormatD
+  cudaArray *cuArray;
+  cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<uchar4>();
+  cudaMallocArray(&cuArray, &channelDesc, width, height);
+  cudaMemcpyToArray(cuArray,0,0,src.data, imgsize, cudaMemcpyHostToDevice);
 
+  //运行时纹理参照系属性
+  tex.addressMode[0] = cudaAddressModeWrap;//循环寻址方式
+  tex.addressMode[1] = cudaAddressModeWrap;
+  tex.filterMode = cudaFilterModeLinear; //线性插值
+  tex.normalized = false;
 
+  //绑定
+  cudaBindTextureToArray(tex,cuArray,channelDesc);
 
+  //output of host and device  
+  cv::Mat out = cv::Mat::zeros(width, height, CV_8UC4);
+  char *dev_out = NULL;
+  cudaMalloc((char**)&dev_out,imgsize);
+
+  //invoke the kernel
+  dim3 block(16, 16);
+  dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y); 
+  smooth_kernel<<<grid, block,0>>>(dev_out,width,height,channels);
+  
+  //copy result from device to host
+  cudaMemcpy(out.data, dev_out, imgsize, cudaMemcpyDeviceToHost);
+  
+  //cv::imshow("origin");
+  //cv::imshow("smooth_image",out);
+  //waitKey(0);
+
+  cv::imwrite("lena_smooth.jpg",out);
+
+  cudaUnbindTexture(tex);
+  cudaFree(dev_out);
+  cudaFree(cuArray);
+
+  return 0;  
 }
