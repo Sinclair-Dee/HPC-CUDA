bdbf14011abaef77e00b3c2c6171fdf407f58765
dixinkai <dixinkai@outlook.com>
2019-04-29 13:39:47 +0000

    update

--- Image_Processing/pic_smooth_texture.cu
+++ Image_Processing/pic_smooth_texture.cu
@@ -13,5 +13,26 @@ __global__
 void smooth_kernel(char *img, int width, int height, int channels){
   unsigned int x = blockIdx.x * blockDim.x + threadIdx.x; 
   unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
-  unsigned int offset = x + y * blockDim.x +
+  unsigned int offset = x + y * blockDim.x * gridDim.x;
+
+  //归一化
+  float u = x/(float)width;
+  float v = y/(float)height;
+
+  //获取纹理内存中的输入图像信息
+  float4 pixel    = tex2D(tex,x,y);
+  float4 left     = tex2D(tex,x-1,y);
+  float4 right    = tex2D(tex,x+1,y);
+  float4 top      = tex2D(tex,x,y-1);
+  float4 botton   = tex2D(tex,x,y+1);
+
+  //获取输出img
+  img[(y*width+x)*channels+0] = (left.x+right.x+top.x+botton.x)/4*255;
+  img[(y*width+x)*channels+1] = (left.y+right.y+top.y+botton.y)/4*255;
+  img[(y*width+x)*channels+2] = (left.z+right.z+top.z+botton.z)/4*255;
+  img[(y*width+x)*channels+3] = 0;
+}
+
+int main(){
+  cv::Mat src = imread("")
 }
