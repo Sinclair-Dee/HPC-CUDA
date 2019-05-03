e6246629d97c5e76ba43a6a042527c1af8d0053d
dixinkai <dixinkai@outlook.com>
2019-05-03 05:51:54 +0000

    update

---
+++ CNN/Net.h
@@ -0,0 +1,35 @@
+/********************************************
+//Net.h
+**********************i**********************/
+
+#ifndef _NET_H_
+#define _NET_H_
+
+//IO
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdio>
+
+//thrust
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+
+//System and Algorithm
+#include <time.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <algorithm>
+
+//String
+#include <cstring>
+#include <string>
+
+namespace GPU_Scope{
+
+}
+
+#endif
