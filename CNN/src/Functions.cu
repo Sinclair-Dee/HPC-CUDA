86d78281fa948567823586afa29e6a2a96cd2dba
dixinkai <dixinkai@outlook.com>
2019-05-03 06:34:10 +0000

    update

---
+++ CNN/src/Functions.cu
@@ -0,0 +1,12 @@
+#include "Net.h"
+
+
+
+
+
+
+
+void clear(){
+  usleep(100000);
+  for(int i = 0; i < 100; i++) printf("\n")
+}
--- CNN/src/Net.h
+++ CNN/src/Net.h
@@ -28,8 +28,29 @@
 #include <cstring>
 #include <string>
 
-namespace GPU_Scope{
+//常量定义区
+#define TILE_WIDTH 32
+#define MINIBATCH 1000
+#define LEARNIG_RATE 2e-4
+#define LAMDA (3e-1)
+#define CONV_KERNEL_SIZE 13
+#define LABEL_ONE 1
+#define LABEL_ZERO 0
+#define NUM_TRAIN 60000
+#define NUM_TEST  10000
+#define RAW_DIM 28
+#define RAW_PIXELS_PER_IMG 784
+#define RAW_DIM_PADDING 32
+#define RAW_PIXEL_PER_IMG_PADDING 1024
+#define MNISI_SCALE_FACTOR 0.00390625
+#define MAXBYTE 255
 
+void ClearScreen();
+
+namespace GPU_Scope{
+  
+
+  
 }
 
 #endif
