0980c35c021e67688495f7c5b82c10da06950f13
dixinkai <dixinkai@outlook.com>
2019-05-13 10:21:52 +0000

    update

---
+++ Mnist/src/Mnist.cu
@@ -0,0 +1,11 @@
+#include "Net.h"
+
+const char* PATH_TRAIN_DATA  = "../mnist/train-images-idx3-ubyte"
+const char* PATH_TRAIN_LABEL = "../mnist/train-labels-idx1-ubyte"
+const char* PATH_TEST_DATA   = "../mnist/t10k-images-idx3-ubyte"
+const char* PATH_TEST_LABEL  = "../mnist/t10k-labels-idx1-ubyte"
+
+/***** Main function ***********************************/
+int main(){
+
+}
