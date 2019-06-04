f8098acaec97bed00b55412e9ab2b43f1ae99735
dixinkai <dixinkai@outlook.com>
2019-06-04 06:30:57 +0000

    update

---
+++ CUDA_by_Example/common/image.h
@@ -0,0 +1,41 @@
+#ifndef __IMAGE__
+#define __IMAGE__
+
+#include <iostream>
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace std;
+
+class IMAGE
+{
+public:
+    Mat image;
+
+    IMAGE( int w, int h) 
+    {
+        image = Mat::zeros(w,h,CV_8UC4);
+        imshow("images",image);
+    }
+
+    unsigned char* get_ptr( void ) const   
+    { 
+        return (unsigned char*)image.data; 
+    }
+
+    long image_size( void ) const 
+    { 
+	return image.cols * image.rows * 4; 
+    }
+
+    char show_image(int time=0)
+    {
+        imshow("images",image);
+        return waitKey(time);
+    }
+};
+
+#endif  // __IMAGE__
